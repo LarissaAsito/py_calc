2bcc94f0814c33668b16a1f090557112a58615ae
larissavqz <larissavasquezz@outlook.com>
2019-10-24 22:30:24 -0300

    PULL1

--- divisao.py
+++ divisao.py
@@ -3,6 +3,6 @@ def Div(a, b):
 		c = a / b #dividindo a por b 
 		return(c) #retorna resultado da divisão
 	else:
-        return("Nao da para dividir por 0!")  
+        return("Nao da para dividir por 0!")  #mostra mensagem
 		
 		
