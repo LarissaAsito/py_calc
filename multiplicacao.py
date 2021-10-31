932122cb8f03e5bebfe4038e5d78b9fc81235d71
larissavqz <larissavasquezz@outlook.com>
2021-10-31 18:56:21 -0300

    Update multiplicacao.py

--- multiplicacao.py
+++ multiplicacao.py
@@ -1,4 +1,3 @@
 def Mult(a, b):
     c = a * b # multiplicando a com b 
-    return (c)#retorna o valor da multiplicacao a*b
-	#fim
+    return (c)#retorna o valor da multiplicacao a*b que foi atribuido a variavel c
